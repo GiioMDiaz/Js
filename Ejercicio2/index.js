36a1849c8dc3697070a9640a9c9a3357fb513e76
Giovanni MD <giovanni.martinez@bosonit.com>
2022-07-14 11:32:53 +0200

    add title

--- Ejercicio2/index.js
+++ Ejercicio2/index.js
@@ -1,3 +1,5 @@
+// Dado un array de valores, devolver un array truthy (sin valores nulos, vacíos, no números, indefinidos o falsos)
+
 const arrDirty = [NaN, 0, 5, false, -1, '',undefined, 3, null, 'test']
 
 console.log(arrDirty.filter(Boolean))
