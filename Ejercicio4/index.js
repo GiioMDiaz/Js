51b3d0e924680a2088d84cf42d8fd0240f86412a
Giovanni MD <giovanni.martinez@bosonit.com>
2022-07-15 13:19:57 +0200

    Ejercicio 4

---
+++ Ejercicio4/index.js
@@ -0,0 +1,12 @@
+/* Dado tres arrays de números, sacar en un nuevo array la intersección de estos. */
+
+const arrNumber1 = [1,2,3];
+const arrNumber2 = [1,2,3,4,5];
+const arrNumber3 = [1,4,7,2];
+
+const inters =() => { 
+   let result = arrNumber1.filter(s => arrNumber2.indexOf(s) !== -1 && arrNumber3.indexOf(s) !== -1)
+    return result
+}
+
+console.log(inters());
