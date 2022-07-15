7291c9f7956772962f19a4445e2116a108dfecc7
Giovanni MD <giovanni.martinez@bosonit.com>
2022-07-15 14:24:31 +0200

    Ejercicio 4 v2

--- Ejercicio4/index.js
+++ Ejercicio4/index.js
@@ -4,9 +4,8 @@ const arrNumber1 = [1,2,3];
 const arrNumber2 = [1,2,3,4,5];
 const arrNumber3 = [1,4,7,2];
 debugger
-const inters =() => { 
-   let result = arrNumber2.filter(s => arrNumber1.indexOf(s) !== -1 && arrNumber3.indexOf(s) !== -1)
-    return result
-}
+const inters =() => arrNumber2.filter(s => arrNumber1.indexOf(s) !== -1 && arrNumber3.indexOf(s) !== -1)
+    
+
 
 console.log(inters());
