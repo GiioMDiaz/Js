3e67c2eacad704e30c74f19aa5ea54b2cef39af8
Giovanni MD <giovanni.martinez@bosonit.com>
2022-07-16 15:06:43 +0200

    Ejercicio 3 v2

--- Ejercicio3/index.js
+++ Ejercicio3/index.js
@@ -1,17 +1,15 @@
 // Dado un array de ciudades, sacar todas las ciudades de España que no sean capitales
 
 const arrCities = [
-    {city: 'Logroño', country: 'Spain', capital: false},
-    {city: 'Paris', country: 'France', capital: true},
-    {city: 'Madrid', country: 'Spain', capital: true},
-    {city: 'Rome', country: 'Italy', capital: true},
-    {city: 'Oslo', country: 'Norway', capital: true},
-    {city: 'Jaén', country: 'Spain', capital: false}
-  ]
-  
-  var index
-  for(index = 0; index <= arrCities.length -1; index++){
-    if(arrCities[index].capital===false){
-          console.log(arrCities[index].city)
-    }
-  }
+  { city: 'Logroño', country: 'Spain', capital: false },
+  { city: 'Paris', country: 'France', capital: true },
+  { city: 'Madrid', country: 'Spain', capital: true },
+  { city: 'Rome', country: 'Italy', capital: true },
+  { city: 'Oslo', country: 'Norway', capital: true },
+  { city: 'Jaén', country: 'Spain', capital: false }
+]
+
+const arr = () => arrCities.filter((item) => !item.capital)
+
+
+console.log(arr())
