fc13bea9c59b10a4333f1d9f21df0a840dca02b4
Giovanni MD <giovanni.martinez@bosonit.com>
2022-07-19 12:23:55 +0200

    Refactor

--- Ejercicio7/index.js
+++ Ejercicio7/index.js
@@ -1,21 +1,21 @@
 /* Crea una función que retorne los campos de un objeto que equivalgan a un valor “falsy” 
 después de ser ejecutados por una función específica.
 La fundación debe tener dos parámetros:
-
 ***Primer parámetro es un objeto con x número de campos y valores
-
 ***Segundo parametro es una funcion que retorne un booleano, que se tiene que aplicar al objeto del primer parámetro */
 
-
-
-const notBol = (arr, fn) => {
-
-    let filt = arr.filter(x => !x);
-
-    return filt
-
+function returnFalsy(arr, type) {
+    debugger
+    for (var values in arr) {
+        if (type(arr[values])) {
+            delete arr[values];
+        }
+    }
+    return arr
 }
 
-const arr = [null, undefined, "Hola", "ate", "", false, 9, 0, NaN];
+const arr = { a: 1, b: '2', c: 3 }
+const type = (x => typeof x === "string")
 
-console.log(notBol(arr, n => n));
+
+console.log(returnFalsy(arr, type));
